a14678afb043b00b8c569a8fa5f2b396a6e5858d
schleichardt <michael.schleichardt@commercetools.de>
2015-02-17 13:48:20 +0100

    Document thread-safety of the client. #171

--- java-client-core/src/main/java/io/sphere/sdk/client/SphereClient.java
+++ java-client-core/src/main/java/io/sphere/sdk/client/SphereClient.java
@@ -18,6 +18,16 @@ import java.util.concurrent.CompletableFuture;
  *
  */
 public interface SphereClient extends Closeable {
+    /**
+     * Performs one request to the SPHERE.IO API, this includes the sending the HTTP request and transform the resulting JSON into a Java object.
+     *
+     * <p>This method should not throw exceptions, the exceptions should be in the {@link java.util.concurrent.CompletableFuture}.</p>
+     * The method is thread-safe.
+     *
+     * @param sphereRequest the description of the request and the response transformation
+     * @param <T> the type of the resulting Java object if no exception occurs
+     * @return the result embedded into a future, the result can be the object or an exception
+     */
     <T> CompletableFuture<T> execute(final SphereRequest<T> sphereRequest);
 
     void close();
